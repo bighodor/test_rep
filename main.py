3978c1a11f9e7e72ba5fbd0bd1fc19eb8cf4058b
EgorVolkov <Bighodor@mail.ru>
2021-11-19 15:27:57 +0300

    Last changes

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # Главный файл проекта
 from file2 import data
 from file3 import another_data
+import math as m
 
 
 def func_main(data, another_data):
+    buf = m.sin(2)
     res = []  # Список для хранения результатов
     for element in data:
         if element in another_data:
             res.append(element)  # Добавление элемента в результирующий список
+
     return element
 
 
